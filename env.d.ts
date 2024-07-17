957c922f2bf614695ef7faf41189cdb96ed999f4
Alexandr Sachyshyn <alexandr.sachyshyn@gmail.com>
2024-07-17 14:56:35 +0200

    feat: updated lint and format commands, changed end of line in prettier to crlf

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv extends Readonly<Record<string, string>> {
-  readonly APP_API_URL: string
+  readonly APP_API_URL: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
