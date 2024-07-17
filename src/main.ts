c795f792ef6dba90365e9b3293f835d3b2c9cca8
Alexandr Sachyshyn <alexandr.sachyshyn@gmail.com>
2024-07-17 11:10:36 +0200

    refactor: added blank core fsd structure without widgets layer

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(
+  createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: []
+  })
+)
 
 app.mount('#app')
