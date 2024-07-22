4345ad68fd873634dc2fb49b924f46219d789f8c
Alexandr Sachyshyn <alexandr.sachyshyn@gmail.com>
2024-07-22 11:48:41 +0200

    feat: added feature to set a port in .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,53 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { type PreRenderedAsset } from 'rollup';
 
-export default defineConfig({
-  plugins: [vue()],
-  envPrefix: 'APP_',
-  server: {
-    host: true,
-    port: 8080
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo: PreRenderedAsset) => {
-          if (!assetInfo.name) {
+export default defineConfig(({ mode }) => {
+  const ENV_PREFIX = 'APP_';
+  const env = loadEnv(mode, process.cwd(), ENV_PREFIX);
+  const DEFAULT_PORT = 3000;
+
+  return {
+    plugins: [vue()],
+    envPrefix: ENV_PREFIX,
+    server: {
+      host: true,
+      port: env.APP_PORT ? Number(env.APP_PORT) : DEFAULT_PORT
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo: PreRenderedAsset) => {
+            if (!assetInfo.name) {
+              return 'assets/[name]-[hash][extname]';
+            }
+
+            const extType = assetInfo.name.split('.').pop()!;
+
+            if (/css/.test(extType)) {
+              return 'assets/styles/[name]-[hash][extname]';
+            }
+
+            if (/woff|woff2|eot|ttf|otf/.test(extType)) {
+              return 'assets/fonts/[name][extname]';
+            }
+
+            if (/png|jpe?g|svg|gif|ico|webp|avif/.test(extType)) {
+              return 'assets/images/[name][extname]';
+            }
+
             return 'assets/[name]-[hash][extname]';
-          }
-
-          const extType = assetInfo.name.split('.').pop()!;
-
-          if (/css/.test(extType)) {
-            return 'assets/styles/[name]-[hash][extname]';
-          }
-
-          if (/woff|woff2|eot|ttf|otf/.test(extType)) {
-            return 'assets/fonts/[name][extname]';
-          }
-
-          if (/png|jpe?g|svg|gif|ico|webp|avif/.test(extType)) {
-            return 'assets/images/[name][extname]';
-          }
-
-          return 'assets/[name]-[hash][extname]';
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js'
+          },
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          entryFileNames: 'assets/js/[name]-[hash].js'
+        }
       }
     }
-  }
+  };
 });
