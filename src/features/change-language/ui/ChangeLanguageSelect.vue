099e42eb9b5a8dd5ce8a1325d43f0d1d8559d38d
Alexandr Sachyshyn <alexandr.sachyshyn@gmail.com>
2024-07-23 15:13:01 +0200

    feat: added a label to the language select

--- src/features/change-language/ui/ChangeLanguageSelect.vue
+++ src/features/change-language/ui/ChangeLanguageSelect.vue
@@ -1,9 +1,18 @@
 <template>
-  <select name="i18n-lang-changer" v-model="languageModel">
-    <option v-for="locale of translationService.availableLocales" :key="locale" :value="locale">
-      {{ locale }}
-    </option>
-  </select>
+  <label for="i18n-lang-changer">
+    <span>{{ $t('common.current_language') }}</span>
+
+    <select
+      :aria-label="$t('common.change_language')"
+      name="i18n-lang-changer"
+      id="i18n-lang-changer"
+      v-model="languageModel"
+    >
+      <option v-for="locale of translationService.availableLocales" :key="locale" :value="locale">
+        {{ locale }}
+      </option>
+    </select>
+  </label>
 </template>
 
 <script setup lang="ts">
@@ -12,3 +21,9 @@ import { useLanguage } from '../model';
 
 const languageModel = useLanguage();
 </script>
+
+<style scoped>
+#i18n-lang-changer {
+  margin-left: 0.5rem;
+}
+</style>
