3b002a3acdef970c24de267bb1ae11cd73250eee
Alexandr Sachyshyn <alexandr.sachyshyn@gmail.com>
2024-07-29 11:59:35 +0200

    feat: removed unused APP_DEFAULT_LOCALE from .env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,7 @@
 /// <reference types="vite/client" />
 
-import type { Locale } from '@/shared/lib';
-
 interface ImportMetaEnv extends Readonly<Record<string, string>> {
   readonly APP_API_URL: string;
-  readonly APP_DEFAULT_LOCALE: Locale;
 }
 
 interface ImportMeta {
