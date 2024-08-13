73a4ac108ddbacb5436ea96a01e192b1e9d98dc1
sachyshyn <alexandr.sachyshyn@gmail.com>
2024-08-13 14:48:09 +0200

    feat: enhanced env absence error

--- src/shared/config/env/index.ts
+++ src/shared/config/env/index.ts
@@ -2,7 +2,34 @@ function getEnvironmentVariable(key: string) {
   const environmentVariable = import.meta.env[key];
 
   if (environmentVariable === undefined) {
-    throw new Error(`Environment variable "${key}" is not set`);
+    const errorTextWrapper = document.createElement('div');
+
+    const errorHeading = document.createElement('h1');
+    errorHeading.textContent = 'Something went wrong';
+
+    const errorDescription = document.createElement('p');
+    errorDescription.textContent =
+      'An error occurred while starting the application. Please, check logs in the console';
+
+    Object.assign(errorTextWrapper.style, {
+      display: 'flex',
+      flexDirection: 'column',
+      width: '100%',
+      minHeight: '100vh',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: '1rem'
+    });
+
+    errorTextWrapper.appendChild(errorHeading);
+    errorTextWrapper.appendChild(errorDescription);
+
+    document.body.innerHTML = '';
+    document.body.appendChild(errorTextWrapper);
+
+    const errorMessage = `Critical Error: required environment variable "${key}" is not set`;
+
+    throw new Error(errorMessage);
   }
 
   return environmentVariable;
